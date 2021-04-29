0a7c73b53f87aedccbac7737967931a5018cd405
Ann-Christin Frank <ann-christin.frank@trixie.de>
2021-04-29 07:13:28 +0200

    VPPSSnacks - daten werden ausgegeben

--- src/Components/Templates/ContentElements/Textmedia.tsx
+++ src/Components/Templates/ContentElements/Textmedia.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Textmedia: React.FC<{ data: any }> = props => {
 console.log(props.data)
-    return <div className="textmedia">
-        <div className="textmedia textmedia-gallery">
+    return
+        <div className="textmedia">
+            <div className="textmedia textmedia-gallery">
             { /* <div className="textmedia-item textmedia-gallery"> */ }
-            </div>
+             </div>
             <div className="textmedia-item textmedia-text">
                 { /* <f:render partial="Header/All" arguments="{_all}"/> */ }
                 <div dangerouslySetInnerHTML={{__html: props.data.bodytext}} />
