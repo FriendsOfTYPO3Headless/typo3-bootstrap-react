b0adb58c9339870f202cd8f3a4fc166dfaaa6983
Niels Seelhöfer <niels.seelhoefer@trixie.de>
2021-12-03 14:37:02 +0100

    deleted storybook

--- build/Components/Templates/ContentElements/ImageLightbox.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-declare const ImageLightbox: React.FC<{
-    images: Array<any>;
-    showLightbox: boolean;
-    setShowLightbox: (showLightbox: boolean) => void;
-    photoIndex: number;
-    setPhotoIndex: (value: (((prevState: number) => number) | number)) => void;
-}>;
-export default ImageLightbox;
--- build/stories/Button.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta } from '@storybook/react/types-6-0';
-declare const _default: Meta<import("@storybook/react/types-6-0").Args>;
-export default _default;
-export declare const Primary: any;
-export declare const Secondary: any;
-export declare const Large: any;
-export declare const Small: any;
