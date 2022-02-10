adf28d0510dad32f94f10136e52b6054c359b92f
Niels Seelhöfer <niels.seelhoefer@trixie.de>
2022-02-10 16:48:26 +0100

    Change to v1.0 of friendsoftypo3headless/headless_backendlayouts

--- src/Components/Templates/Page/GenericPage.tsx
+++ src/Components/Templates/Page/GenericPage.tsx
@@ -4,34 +4,34 @@ import Content from "../Content";
 import TYPO3PageContext from "../../../Context/TYPO3PageContext";
 
 interface gridElementInterface {
-    tag: string | any,
+    tag?: string | any | null,
     type: string,
     children?: gridElementInterface[],
     contentColPos?: string,
     colPos: string,
-    size: number,
+    colspan: number,
 }
 
-const getGridElement = (element: gridElementInterface, content: any, contentElementLayouts : any, contentElementTemplates: any, index: number) => {
+const getGridElement = (element: gridElementInterface, content: any, contentElementLayouts: any, contentElementTemplates: any, index: number) => {
     switch (element.type) {
         case 'row':
-            const children = element.children.map((child: gridElementInterface, index:number) => {
+            const children = element.children.map((child: gridElementInterface, index: number) => {
                 return getGridElement(child, content, contentElementLayouts, contentElementTemplates, index);
             });
 
-            return <Row as={element.tag} key={index}>
+            return <Row as={element.tag ?? 'div'} key={index}>
                 {children}
             </Row>
         case 'col':
             return <Col
-                as={element.tag}
-                lg={element.size}
-                md={element.size}
-                sm={element.size} //TODO: raus?
-                xl={element.size}
+                as={element.tag ?? 'div'}
+                lg={element.colspan}
+                md={element.colspan}
+                sm={element.colspan} //TODO: raus?
+                xl={element.colspan}
                 key={index}
             >
-                <Content colPos={element.colPos} />
+                <Content colPos={element.colPos}/>
             </Col>
 
         default:
@@ -42,8 +42,8 @@ const getGridElement = (element: gridElementInterface, content: any, contentElem
 const GenericPage: React.FC = () => {
     const context = React.useContext(TYPO3PageContext);
     let content = <></>
-    if(context.headlessData.appearance.pageContentRows) {
-        content = context.headlessData.appearance.pageContentRows.map((gridElement:any, index: number) =>  {
+    if (context.headlessData.appearance.pageContentRows) {
+        content = context.headlessData.appearance.pageContentRows.map((gridElement: any, index: number) => {
             return getGridElement(gridElement, context.headlessData.content, context.contentElementLayouts, context.contentElementTemplates, index);
         });
     }
