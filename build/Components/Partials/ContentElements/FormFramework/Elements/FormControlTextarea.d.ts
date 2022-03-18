d3b06a57b6a15d2343c76ee06c47f823d2212869
Sören Nissen <soeren.nissen@trixie.de>
2022-03-18 13:17:54 +0100

    Rendered option added and renaming of compoments for better understand

---
+++ build/Components/Partials/ContentElements/FormFramework/Elements/FormControlTextarea.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { TYPO3ContentElementBaseInterface } from "../../../../Interfaces";
+declare const FormControlTextarea: React.FC<TYPO3ContentElementBaseInterface>;
+export default FormControlTextarea;
