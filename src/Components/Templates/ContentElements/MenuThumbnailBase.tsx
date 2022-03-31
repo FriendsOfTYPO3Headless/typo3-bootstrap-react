c0436ece220087c79cb3d7bbb7af13e54674b37f
Niels Seelhöfer <niels.seelhoefer@trixie.de>
2022-03-31 15:54:28 +0200

    menu_card_dir, menu_card_list, menu_thumbnail_dir und menu_thumbnail_list, menu_categorized_pages with same structure

--- src/Components/Templates/ContentElements/MenuThumbnailBase.tsx
+++ src/Components/Templates/ContentElements/MenuThumbnailBase.tsx
@@ -6,28 +6,25 @@ import Image from "../../Partials/ContentElements/Media/Type/Image"
 const MenuThumbnailBase: React.FC<TYPO3BootstrapContentElementBaseInterface> = props => {
     const {flexform, content} = props.data
     const {items} = content
-    const itemsTemplate = items.map((item,index) => {
-        const {title, subtitle, link, target, media} = item
-        if (!media || media.length <= 0) {
-            return <React.Fragment key={`${title}-${index}`}/>
-        }
+    const itemsTemplate = items.map((item, index) => {
+        const {title, link, target, thumbnail} = item //subtitle
+
         return <div key={link} className="thumbnail-menu-item">
             <a href={link} target={target} title={title}
                data-toggle={"tooltip"} className={'thumbnail-menu-link'}>
-                {media && media.length > 0 &&
                     <span className={'thumbnail-menu-image'}>
-                        <Image file={media[0]}/>
-                    </span>
+                {thumbnail && thumbnail.length > 0 &&
+                    <Image file={thumbnail[0]}/>
                 }
+                    </span>
                 <span className={'thumbnail-menu-caption'}>
                     <span className={'thumbnail-menu-caption-inner'}>
                         {title && title.length > 0 &&
                             <span className={'h3 thumbnail-menu-caption-title'}>{title}</span>
                         }
-                        {subtitle && subtitle.length > 0 &&
-                            <p className={'thumbnail-menu-caption-subtitle'}>{subtitle}</p>
-                        }
-
+                        {/*{subtitle && subtitle.length > 0 &&*/}
+                        {/*    <p className={'thumbnail-menu-caption-subtitle'}>{subtitle}</p>*/}
+                        {/*}*/}
                     </span>
                 </span>
             </a>
@@ -36,9 +33,11 @@ const MenuThumbnailBase: React.FC<TYPO3BootstrapContentElementBaseInterface> = p
     })
     return <>
         <AllHeader data={props.data}/>
-        <div className={`thumbnail-menu thumbnail-menu-align-${flexform.align} thumbnail-menu-columns-${flexform.columns}`}>
-        {itemsTemplate}
-    </div></>
+        <div
+            className={`thumbnail-menu thumbnail-menu-align-${flexform.align} thumbnail-menu-columns-${flexform.columns}`}>
+            {itemsTemplate}
+        </div>
+    </>
 }
 
 
